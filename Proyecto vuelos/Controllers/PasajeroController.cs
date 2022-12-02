95e872f43c74fcfc5db20473a7aab9ec288ec860
AngelFierros <angel.fierros228695@potros.itson.edu.mx>
2022-12-02 13:04:17 -0700

    Se agrego la clase Pasajero con sus metodos y la vista de Pasajero

---
+++ Proyecto vuelos/Controllers/PasajeroController.cs
@@ -0,0 +1,38 @@
+﻿using Proyecto_vuelos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_vuelos.Controllers
+{
+    public class PasajeroController : Controller
+    {
+        // GET: Pasajero
+        public ActionResult Index()
+        {
+            List<Pasajero> pasajeros = Pasajero.GetAll();
+            return View(pasajeros);
+        }
+
+        public ActionResult Registro(int id)
+        {
+            Pasajero pasajero = Pasajero.GetById(id);
+            return View(pasajero);
+        }
+
+        public ActionResult Guardar(int id, string nombre, string apellidoPaterno, string apellidoMaterno, string fechaNacimiento)
+        {
+            Pasajero.Guardar(id, nombre, apellidoPaterno, apellidoMaterno,  fechaNacimiento);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            Avion.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
